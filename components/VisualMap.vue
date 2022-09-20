e4260be387b90b3b0f16be574459b200f815b9de
WillCQ97 <willian.cqueiroz@gmail.com>
2022-09-20 08:16:45 -0300

    Refinamentos componente do mapa e features alegre

--- components/VisualMap.vue
+++ components/VisualMap.vue
@@ -1,26 +1,38 @@
+<!--todo: receber os dados por props -->
+<!--todo: fazer os testes para as informações por resumo -->
 <template>
   <v-row>
-    <v-col>
-      <v-card elevation="8" max-width="100vh" min-width="60vh">
+    <v-spacer />
+    <v-col cols="10">
+      <v-card elevation="8" min-width="80vh">
         <client-only>
+          <!-- PROPRIEDADES DO MAPA -->
           <l-map
             :center="center"
             :options="mapOptions"
             style="height: 600px; z-index: 1"
             :zoom="zoom"
           >
+            <!-- LAYER DO OPENSTREETMAP -->
             <l-tile-layer :attribution="attribution" :url="url"></l-tile-layer>
+
+            <!-- FEATURES DO CAMPUS -->
+            <l-geo-json
+              v-if="showCampusFeature"
+              :geojson="feature"
+              :options="jsonOptions"
+            />
           </l-map>
         </client-only>
       </v-card>
     </v-col>
+    <v-spacer />
   </v-row>
 </template>
 
 <script>
 export default {
   name: 'VisualMapComponent',
-  /*
   props: {
     bounds: {
       type: Array,
@@ -30,33 +42,60 @@ export default {
       type: Array,
       required: true,
     },
-    geojson: {
+    feature: {
       type: Object,
       required: true,
     },
+    /*
     projects: {
       type: Array,
       required: true,
-    },
+    }, */
   },
-  */
   data() {
     return {
       attribution:
         '<a href="https://mapa.prodesignufes.org">Prodesing UFES</a> | &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      bounds: [
-        [-20.75885, -41.53911],
-        [-20.76464, -41.53211],
-      ],
-      center: [-20.76161, -41.536],
+      enableTooltip: true,
       markerIconUrl: 'img/logos/ods-small.png',
       markerIconSize: [20, 20],
       markers: [],
+      showCampusFeature: true,
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       zoom: 18,
     }
   },
   computed: {
+    jsonOptions() {
+      return {
+        onEachFeature: this.onEachFeature,
+      }
+    },
+    onEachFeature() {
+      if (!this.enableTooltip) {
+        return
+      }
+      return (feature, layer) => {
+        const props = feature.properties
+        let tags = '';
+
+        Object.entries(props.palavras_chave).forEach(
+          ([key, value]) => {
+            tags += value + '; '
+          }
+        )
+
+        const popupContent =
+          'ID: ' +
+          props.idd +
+          '<br /> Nome: ' +
+          props.primario +
+          '<br /> Tags: ' +
+          tags
+
+        layer.bindTooltip(popupContent, { permanent: false, sticky: true })
+      }
+    },
     mapOptions() {
       return {
         minZoom: 16,
@@ -64,6 +103,11 @@ export default {
         zoomControl: true,
         scrollWheelZoom: false,
         maxBounds: this.bounds,
+      }
+    },
+    popupOptions() {
+      return {
+        maxWidth: 315,
       }
     },
   },
--- pages/actions/alegre/index.vue
+++ pages/actions/alegre/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <VisualMap />
     <h1>Página para alegre</h1>
     <p>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ut
@@ -12,11 +11,28 @@
       vitae accusamus repudiandae saepe quos. Facilis perferendis nemo fugiat
       quasi quos autem neque quidem, tenetur totam eaque at qui pariatur.
     </p>
+    <!--TODO: COLOCAR ALGUNS CARDS INFORMATIVOS SOBRE OS PROJETOS DE FORMA GERAL
+    O QUE SERIA OUTRO COMPONENTE (DASHBOARD?) -->
+    <VisualMap :bounds="bounds" :center="center" :feature="feature"/>
   </div>
 </template>
 
 <script>
+import alegreFeature from '~/assets/features/alegre_min.json';
+
 export default {
   name: 'AlegrePage',
+
+  data() {
+    return {
+      // TODO: UTILIZAR UMA ÁREA MENOR EM VOLTA DO CAMPUS
+      bounds: [
+        [-20.75885, -41.53911],
+        [-20.76464, -41.53211],
+      ],
+      center: [-20.76161, -41.536],
+      feature: alegreFeature,
+    }
+  },
 }
 </script>
