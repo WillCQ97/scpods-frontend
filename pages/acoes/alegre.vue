cfc459fb7a66c6db6f2255754edc83965207b368
WillCQ97 <willian.cqueiroz@gmail.com>
2022-09-23 15:55:33 -0300

    Carregamento de mapas em diferentes páginas para alegre

---
+++ pages/acoes/alegre.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <!-- TODO: CARREGAR O TEXTO PARA ALEGRE AQUI-->
+    <h1>Página de Wrapper de alegre</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ut
+      laudantium iusto, magnam culpa ipsam, alias amet natus dolore error ad quo
+      eaque, reiciendis labore in optio iure. Unde, incidunt.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi rerum
+      vitae accusamus repudiandae saepe quos. Facilis perferendis nemo fugiat
+      quasi quos autem neque quidem, tenetur totam eaque at qui pariatur.
+    </p>
+
+    <v-card>
+      <v-card-title>Selecione o mapa a ser visualizado</v-card-title>
+      <v-card-actions>
+        <v-btn @click="showMapAlegreSede">Alegre Sede</v-btn>
+        <v-btn @click="showMapJeronimo">Jerônimo Monteiro</v-btn>
+        <v-btn @click="showMapRive">Área Experimental Rive</v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <nuxt-child />
+    <!--TODO: COLOCAR ALGUNS CARDS INFORMATIVOS SOBRE OS PROJETOS DE FORMA GERAL
+    O QUE SERIA OUTRO COMPONENTE (DASHBOARD?) -->
+    <!--
+    -->
+    <!-- TODO: adicionar a lista de projetos a serem exibidos (componente próprio) -->
+    <p>CARREGAR UMA LISTA COM OS PROJETOS A PARTIR DA INTERAÇÃO COM O MAPA</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlegreActionsWrapperPage',
+
+  data() {
+    return {}
+  },
+
+  methods: {
+    showMapAlegreSede() {
+      this.$router.push('/acoes/alegre/sede/')
+    },
+    showMapJeronimo() {
+      this.$router.push('/acoes/alegre/jeronimo/')
+    },
+    showMapRive() {
+      this.$router.push('/acoes/alegre/rive/')
+    },
+  },
+}
+</script>
+
---
+++ pages/acoes/alegre/jeronimo/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <VisualMap
+      title="Jerônimo Monteiro"
+      :bounds="jeronimoBounds"
+      :center="jeronimoCenter"
+      :feature="jeronimoFeature"
+      :markers="createMarkersResumed"
+    />
+  </div>
+</template>
+
+<script>
+import jeronimoFeature from '~/assets/features/jeronimo_min.json'
+
+export default {
+  name: 'AlegreJeronimoMapPage',
+
+  data() {
+    return {
+      jeronimoCenter: [-20.78981, -41.38849],
+      jeronimoFeature,
+      riveBounds: [
+        [-20.7492301, -41.4905947],
+        [-20.7535765, -41.4855781],
+      ],
+    }
+  },
+
+  computed: {
+    // TODO: carregar os marcadores para jerônimo
+    createMarkersResumed() {
+      return []
+    },
+  },
+}
+</script>
