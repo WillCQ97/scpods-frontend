21f8e044769a3cfa40e95b6ba949342a02108bb4
WillCQ97 <willian.cqueiroz@gmail.com>
2022-09-24 16:40:50 -0300

    ajustes gerais na exibição dos mapas

--- pages/acoes/alegre.vue
+++ pages/acoes/alegre.vue
@@ -1,40 +1,67 @@
+<!--TODO: COLOCAR ALGUNS CARDS INFORMATIVOS SOBRE OS PROJETOS DE FORMA GERAL O QUE SERIA OUTRO COMPONENTE (DASHBOARD?) -->
 <template>
-  <div>
-    <!-- TODO: CARREGAR O TEXTO PARA ALEGRE AQUI-->
-    <h1>Página de Wrapper de alegre</h1>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ut
-      laudantium iusto, magnam culpa ipsam, alias amet natus dolore error ad quo
-      eaque, reiciendis labore in optio iure. Unde, incidunt.
-    </p>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi rerum
-      vitae accusamus repudiandae saepe quos. Facilis perferendis nemo fugiat
-      quasi quos autem neque quidem, tenetur totam eaque at qui pariatur.
-    </p>
+  <v-row>
+    <v-col>
+      <!-- TEXTO DA PÁGINA -->
+      <v-row>
+        <v-col>
+          <h1>Página de Wrapper de alegre</h1>
+          <!-- TODO: ADICIONAR O TEXTO PARA ALEGRE AQUI -->
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ut
+            laudantium iusto, magnam culpa ipsam, alias amet natus dolore error
+            ad quo eaque, reiciendis labore in optio iure. Unde, incidunt.
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+            rerum vitae accusamus repudiandae saepe quos. Facilis perferendis
+            nemo fugiat quasi quos autem neque quidem, tenetur totam eaque at
+            qui pariatur.
+          </p>
+        </v-col>
+      </v-row>
 
-    <v-card>
-      <v-card-title>Selecione o mapa a ser visualizado</v-card-title>
-      <v-card-actions>
-        <v-btn @click="showMapAlegreSede">Alegre Sede</v-btn>
-        <v-btn @click="showMapJeronimo">Jerônimo Monteiro</v-btn>
-        <v-btn @click="showMapRive">Área Experimental Rive</v-btn>
-      </v-card-actions>
-    </v-card>
+      <!-- MENU DE ESCOLHA DO MAPA -->
+      <v-row>
+        <v-spacer />
+        <v-col>
+          <v-card>
+            <v-card-title>Escolha o mapa que deseja ver</v-card-title>
+            <!-- TODO: no momento do clique centralizar no mapa -->
+            <v-card-actions>
+              <v-btn @click="showMapAlegreSede">Alegre Sede</v-btn>
+              <v-btn @click="showMapJeronimo">Jerônimo Monteiro</v-btn>
+              <v-btn @click="showMapRive">Área Experimental Rive</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-spacer />
+      </v-row>
 
-    <nuxt-child />
-    <!--TODO: COLOCAR ALGUNS CARDS INFORMATIVOS SOBRE OS PROJETOS DE FORMA GERAL
-    O QUE SERIA OUTRO COMPONENTE (DASHBOARD?) -->
-    <!--
-    -->
-    <!-- TODO: adicionar a lista de projetos a serem exibidos (componente próprio) -->
-    <p>CARREGAR UMA LISTA COM OS PROJETOS A PARTIR DA INTERAÇÃO COM O MAPA</p>
-  </div>
+      <!-- EXIBIÇÃO DO MAPA -->
+      <v-row>
+        <v-col>
+          <nuxt-child />
+        </v-col>
+      </v-row>
+
+      <!-- LISTA DE PROJETOS -->
+      <v-row>
+        <v-col>
+          <!-- TODO: adicionar a lista de projetos a serem exibidos (componente próprio) -->
+          <actions-list />
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import ActionsList from '~/components/ActionsList.vue'
+
 export default {
   name: 'AlegreActionsWrapperPage',
+  components: { ActionsList },
 
   data() {
     return {}
