8d7c154fc538d91d5f58e1e9cf46b8a3a22ef8d6
WillCQ97 <willian.cqueiroz@gmail.com>
2022-10-11 11:16:30 -0300

    Refinamento criação dos marcadores de resumo

--- pages/acoes/alegre/jeronimo/index.vue
+++ pages/acoes/alegre/jeronimo/index.vue
@@ -27,31 +27,7 @@ export default {
 
   computed: {
     createMarkersResumed() {
-      const markers = this.$store.getters.loadedAlegreInfo.jeronimo.map(
-        (resumo) => ({
-          id: resumo.id,
-          coord: resumo.local.coord,
-          content:
-            '<div class="popup">' +
-            '<img class="popup_img" src="' +
-            require('~/assets/ods_icons/' + resumo.id_ods_principal + '.png') +
-            '"><br>' +
-            '<div class="popup_text">' +
-            '<strong>' +
-            resumo.local.nome +
-            '</strong>' +
-            '<br/>Total de Projetos: ' +
-            resumo.qtd_projetos_totais +
-            '<br/>Total de Projetos Ativos: ' +
-            resumo.qtd_projetos_ativos +
-            '<br/>Total de ODS atendidos: ' +
-            resumo.qtd_ods +
-            '<br/>ODS Principal Atendido: ' +
-            resumo.id_ods_principal +
-            '</div></div>',
-        })
-      )
-      return markers
+      return this.$store.getters.createMarkersInfoAlegre('jeronimo')
     },
   },
 }
