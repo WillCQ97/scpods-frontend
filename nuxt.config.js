4fa09613cba3abc65747537cbd12b861426ad503
WillCQ97 <willian.cqueiroz@gmail.com>
2023-09-25 20:26:20 -0300

    (core) migração: atualização da propriedade head

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,40 +2,42 @@ import { defineNuxtConfig } from '@nuxt/bridge'
 import colors from 'vuetify/es5/util/colors'
 
 // TODO: adicionar imagem e url para o site
-const siteTitle = 'Mapa Colaborativo: ODS na UFES'
+const siteTitle = 'Mapa Colaborativo: ODS na UFES';
 const siteDescription =
-  'Conheça as ações e projetos relacionados com os Objetivos de Desenvolvimento Sustentável (ODS) na UFES'
+'Conheça as ações e projetos relacionados com os Objetivos de Desenvolvimento Sustentável (ODS) na UFES'
 const siteImage = ''
 const siteUrl = ''
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: siteTitle,
+      titleTemplate: '%s',
+      htmlAttrs: {
+        lang: 'pt-br',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'format-detection', content: 'telephone=no' },
+
+        // Open Graph attributes
+        { hid: 'og-type', name: 'og:type', content: 'website' },
+        { hid: 'og-title', name: 'og:title', content: siteTitle },
+        {
+          hid: 'og-description',
+          name: 'og:description',
+          content: siteDescription,
+        },
+        { hid: 'og-image', name: 'og:image', content: siteImage },
+        { hid: 'og-url', name: 'og:url', content: siteUrl },
+        { hid: 'og-locale', name: 'og:locale', content: 'pt_BR' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon-ods.png' }],
+    },
+  },
   bridge: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: '%s',
-    title: siteTitle,
-    htmlAttrs: {
-      lang: 'pt-br',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'format-detection', content: 'telephone=no' },
-
-      // Open Graph attributes
-      { hid: 'og-type', name: 'og:type', content: 'website' },
-      { hid: 'og-title', name: 'og:title', content: siteTitle },
-      {
-        hid: 'og-description',
-        name: 'og:description',
-        content: siteDescription,
-      },
-      { hid: 'og-image', name: 'og:image', content: siteImage },
-      { hid: 'og-url', name: 'og:url', content: siteUrl },
-      { hid: 'og-locale', name: 'og:locale', content: 'pt_BR' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon-ods.png' }],
-  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/styles/main.css', '~/assets/styles/font.css'],
