4877553a0d934a9e72047d83ee22fe866bb03328
WillCQ97 <willian.cqueiroz@gmail.com>
2023-10-30 21:08:22 -0300

    add alegreInfo

--- pages/acoes/alegre/sede/index.vue
+++ pages/acoes/alegre/sede/index.vue
@@ -10,6 +10,7 @@
 
 <script>
 import alegreFeature from '~/assets/features/alegre_min.json'
+import alegreInfo from '~/assets/data/alegre_info.json'
 
 export default {
   name: 'AlegreSedeMapPage',
@@ -43,7 +44,29 @@ export default {
 
   computed: {
     createMarkersResumed() {
-      return this.$store.getters.createMarkersInfoAlegre('sede')
+      const markers = alegreInfo.sede.map((resumo) => ({
+        id: resumo.id,
+        coord: resumo.local.coord,
+        content:
+          '<div class="popup">' +
+          '<img class="popup_img" src="' +
+          require('~/assets/ods_icons/' + resumo.id_ods_principal + '.png') +
+          '"><br>' +
+          '<div class="popup_text">' +
+          '<strong>' +
+          resumo.local.nome +
+          '</strong>' +
+          '<br/>Total de Projetos: ' +
+          resumo.qtd_projetos_totais +
+          '<br/>Total de Projetos Ativos: ' +
+          resumo.qtd_projetos_ativos +
+          '<br/>Total de ODS atendidos: ' +
+          resumo.qtd_ods +
+          '<br/>ODS Principal Atendido: ' +
+          resumo.id_ods_principal +
+          '</div></div>',
+      }))
+      return markers
     },
   },
 }
