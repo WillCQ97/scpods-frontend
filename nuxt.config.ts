a275404e507e476efa9f89c271f7e6f3a8649b82
WillCQ97 <willian.cqueiroz@gmail.com>
2023-11-26 06:38:45 -0300

    Adicionando config para meta, segundo defaults - https://nuxt.com/docs/getting-started/seo-meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,11 +10,42 @@ const siteImage = ''
 const siteUrl = ''
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      htmlAttrs: {
+        lang: 'pt-br',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'format-detection', content: 'telephone=no' },
+
+        // Open Graph attributes
+        { hid: 'og-type', name: 'og:type', content: 'website' },
+        { hid: 'og-title', name: 'og:title', content: siteTitle },
+        {
+          hid: 'og-description',
+          name: 'og:description',
+          content: siteDescription,
+        },
+        { hid: 'og-image', name: 'og:image', content: siteImage },
+        { hid: 'og-url', name: 'og:url', content: siteUrl },
+        { hid: 'og-locale', name: 'og:locale', content: 'pt_BR' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon-ods.png' }],
+      titleTemplate: '%s',
+      title: siteTitle,
+      viewport: 'width=device-width, initial-scale=1',
+    },
+  },
+
   build: {
     transpile: ['vuetify'],
   },
-  
+
+  devtools: { enabled: true },
+
   modules: [
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
