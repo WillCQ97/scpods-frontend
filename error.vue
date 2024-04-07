f2836fa982650ababfc5f592f2088115400a2675
WillCQ97 <willian.cqueiroz@gmail.com>
2024-04-07 19:47:17 -0300

    fix: ajustando página de erro

--- error.vue
+++ error.vue
@@ -1,48 +1,42 @@
 <template>
   <div>
-    <nuxt-layout name="default">
-      <v-app>
-        <v-row>
-          <v-spacer />
-          <v-col>
-            <v-card width="80vh">
-              <v-card-title v-if="error.statusCode === 404">
-                {{ pageNotFound }}
-              </v-card-title>
-              <v-card-title v-else>
-                {{ otherError }} Erro {{ error.statusCode }}
-              </v-card-title>
+    <v-app>
+      <v-row id="card-row">
+        <v-col id="card-col">
+          <v-card width="80vh">
+            <v-card-title v-if="error.statusCode === 404">
+              {{ pageNotFound }}
+            </v-card-title>
+            <v-card-title v-else>
+              {{ otherError }} Erro {{ error.statusCode }}
+            </v-card-title>
 
-              <hr />
-              <v-card-text v-if="error.statusCode === 404">
-                <p>
-                  Lamentamos, mas a página que você está tentando acessar não
-                  existe!
-                </p>
-              </v-card-text>
-              <v-card-text v-else>
-                <p>
-                  Ocorreu um erro inesperado, por favor retorne a página
-                  inicial.
-                </p>
-              </v-card-text>
+            <hr />
+            <v-card-text v-if="error.statusCode === 404">
+              <p>
+                Lamentamos, mas a página que você está tentando acessar não
+                existe!
+              </p>
+            </v-card-text>
+            <v-card-text v-else>
+              <p>
+                Ocorreu um erro inesperado, por favor retorne a página inicial.
+              </p>
+            </v-card-text>
 
-              <v-card-actions>
-                <v-spacer />
-                <!--TODO: trocar para um link normal, pois caso o javascript quebre, possa ser recarregado a partir da página inicial -->
-                <nuxt-link to="/">
-                  <v-btn color="primary" small text>
-                    <v-icon small>mdi-home</v-icon> Página Inicial
-                  </v-btn>
-                </nuxt-link>
-                <v-spacer />
-              </v-card-actions>
-            </v-card>
-          </v-col>
-          <v-spacer />
-        </v-row>
-      </v-app>
-    </nuxt-layout>
+            <v-card-actions>
+              <v-spacer />
+              <a href="/">
+                <v-btn color="primary" small text>
+                  <v-icon small>mdi-home</v-icon> Página Inicial
+                </v-btn>
+              </a>
+              <v-spacer />
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-app>
   </div>
 </template>
 
@@ -75,5 +69,12 @@ export default {
 <style scoped>
 h1 {
   font-size: 20px;
+}
+#card-row {
+  align-items: center;
+}
+#card-col {
+  display: flex;
+  justify-content: center;
 }
 </style>
