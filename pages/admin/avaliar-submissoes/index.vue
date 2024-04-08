f20930240ab4700416cf48f179c9ddfec408311f
WillCQ97 <willian.cqueiroz@gmail.com>
2024-04-07 23:34:22 -0300

    add todo e teste

--- pages/admin/avaliar-submissoes/index.vue
+++ pages/admin/avaliar-submissoes/index.vue
@@ -1,10 +1,21 @@
 <template>
   <v-card>
     <v-card-title>Página de Avaliação de Submissões</v-card-title>
+    <v-card>user.isAdmin</v-card>
   </v-card>
 </template>
 
 <script lang="ts">
+/* <script setup lang="ts"> */
+/*
+ * ESSA DECLARAÇÃO RESULTA EM ERRO SE A PÁGINA ATUAL FOR A PRIMEIRA A SER ACESSADA
+ * [nuxt] error caught during app initialization Error: [🍍]: "getActivePinia()" was called but there was no active Pinia. Are you trying to use a store before calling "app.use(pinia)"?
+ * See https://pinia.vuejs.org/core-concepts/outside-component-usage.html for help.
+ *
+ * AO TROCAR PARA SCRIPT SETUP, O PROBLEMA NÃO OCORRE
+ */
+const user = useUser()
+
 definePageMeta({
   middleware: 'auth',
 })
--- pages/entrar.vue
+++ pages/entrar.vue
@@ -1,12 +1,11 @@
+<!-- TODO: FINALIZAR ESTE TEMPLATE-->
 <template>
   <v-row>
-    <v-spacer />
     <v-col>
       <v-card max-width="80vh">
         <v-card-title>Faça o login</v-card-title>
         <v-card-text>asd</v-card-text>
       </v-card>
     </v-col>
-    <v-spacer />
   </v-row>
 </template>
