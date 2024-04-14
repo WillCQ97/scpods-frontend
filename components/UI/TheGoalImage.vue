b276dfec4ce02bc493ee1bdc87b32a46bf62e850
WillCQ97 <willian.cqueiroz@gmail.com>
2024-04-14 15:17:21 -0300

    feat: criado componente para carregar as images dos objetivos

---
+++ components/UI/TheGoalImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-img :src="loadGoalIcon" height="100" width="100" cover></v-img>
+</template>
+
+<script>
+export default {
+  name: 'TheGoalImageComponent',
+  props: {
+    goalId: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    loadGoalIcon() {
+      return '/img/ods_icons/' + this.goalId + '.png'
+    },
+  },
+}
+</script>
