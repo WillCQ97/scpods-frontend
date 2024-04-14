450d3231e88e5f88ff906e8be025d3e867c2af10
WillCQ97 <willian.cqueiroz@gmail.com>
2024-04-14 12:46:07 -0300

    chore: removendo json de teste

--- components/UI/AppCard.vue
+++ components/UI/AppCard.vue
@@ -1,3 +1,4 @@
+<!-- TODO: Teste para criação de um card base -->
 <template>
   <v-card>
     <v-card-title> {{ card.titulo }} </v-card-title>
@@ -34,6 +35,19 @@ export default {
           link: '',
         },
       },
+      cards: [
+        {
+          titulo: 'A agenda 2030 e os ODS',
+          subtitulo:
+            'Saiba mais sobre os Objetivos de Desenvolvimento Sustentável',
+          texto:
+            'A Agenda 2030 consiste em um plano de ação global para governos, sociedade, empresas, academias e pessoas, no qual foram definidos 17 objetivos e 169 metas para o fomento de um desenvolvimento mais sustentável das atividades humanas nos próximos anos.',
+          botao: {
+            texto: 'Veja mais',
+            link: '',
+          },
+        },
+      ],
     }
   },
 }
