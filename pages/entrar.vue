f3d10c875409ff7775d9f1fa21b494e4fe968ff3
WillCQ97 <willian.cqueiroz@gmail.com>
2024-04-17 14:03:13 -0300

    feat: revisão final
    - criação do template básico página de login
    - remoção da página teste para a listagem de ações
    - add todo

--- pages/entrar.vue
+++ pages/entrar.vue
@@ -1,11 +1,30 @@
 <!-- TODO: FINALIZAR ESTE TEMPLATE-->
 <template>
   <v-row>
+    <v-spacer />
     <v-col>
-      <v-card max-width="80vh">
+      <v-card width="95vh">
         <v-card-title>Faça o login</v-card-title>
-        <v-card-text>asd</v-card-text>
+        <v-card-text>
+          <v-list>
+            <v-list-item>
+              Nós não armazenamos seu login para nenhum propósito
+            </v-list-item>
+            <v-list-item>
+              Esta informação será utilizada apenas para garantir que você faz
+              parte da comunidade acadêmica da UFES.
+            </v-list-item>
+          </v-list>
+          <v-text-field label="Informe seu CPF"></v-text-field>
+          <v-text-field label="Informe sua senha da UFES"></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn> Validar Login </v-btn>
+          <v-btn @click="navigateTo('/')"> Cancelar </v-btn>
+        </v-card-actions>
       </v-card>
     </v-col>
+    <v-spacer />
   </v-row>
 </template>
--- pages/teste.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <action-card-detail-component :is-submission="false" :action="actions[0]" />
-</template>
-
-<script setup lang="ts">
-import ActionCardDetailComponent from '~/components/Actions/ActionCardDetail.vue'
-import actions from '~/assets/data/alegreActions.json'
-</script>
