5bcce0ef57ff1b63b16e87fa76268838a4b4208f
WillCQ97 <willian.cqueiroz@gmail.com>
2024-07-18 23:39:32 -0300

    chore: add códigos salvos em stash e alguns testes

--- stores/objetivo.ts
+++ stores/objetivo.ts
@@ -48,3 +48,7 @@ export const useObjetivoStore = defineStore('objetivoStore', {
     },
   },
 })
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useUserStore, import.meta.hot))
+}
