a3aed92e34da22009fc7643bec95ad9b980eb2a2
WillCQ97 <willian.cqueiroz@gmail.com>
2026-01-22 20:19:01 -0300

    chore: espaços vazios

--- app/repository/factory.ts
+++ app/repository/factory.ts
@@ -2,8 +2,8 @@
 import type { $Fetch, FetchOptions } from 'ofetch'
 
 /*
- The FetchFactory acts as a wrapper around an HTTP client. 
- It encapsulates the functionality for making API requests asynchronously 
+ The FetchFactory acts as a wrapper around an HTTP client.
+ It encapsulates the functionality for making API requests asynchronously
  through the call function, utilizing the provided HTTP client.
 */
 class FetchFactory {
