986ba9308087a50f792c875a4ef578305e2ce4ff
WillCQ97 <willian.cqueiroz@gmail.com>
2026-03-17 22:44:36 -0300

    chore: pre-bundle @vue-leaflet in nuxt.config.ts
    - Isso corrige um antigo problema de desenvolvimento que fazia a página ser carregada quando o botão que exibe o mapa era clicado

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,7 @@ export default defineNuxtConfig({
         '@vue/devtools-core',
         '@vue/devtools-kit',
         'buffer', // CJS
+        '@vue-leaflet/vue-leaflet',
       ],
     },
     vue: {
